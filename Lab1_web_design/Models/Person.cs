649d135df0f4689c5b687802b65f427636ce90ab
Coryna.Johnson <corynamarie@gmail.com>
2016-10-01 10:19:48 -0700

    added step person class

---
+++ Lab1_web_design/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1_web_design.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
