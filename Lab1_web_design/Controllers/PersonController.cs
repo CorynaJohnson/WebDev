ffaac06b676e0b0cd69d6d61148c9759d092ab65
Coryna.Johnson <corynamarie@gmail.com>
2016-10-01 09:53:00 -0700

    first commit

---
+++ Lab1_web_design/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1_web_design.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+            ViewBag.Name = "John Doe";
+            ViewBag.Address = "1230 NE Test Ave";
+            return View();
+        }
+    }
+}
---
+++ Lab1_web_design/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab1_web_design.MvcApplication" Language="C#" %>
